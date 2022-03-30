5e9c32c94228d04359d6fc44a2941e2138296eec
rotem <rotemg38@gmail.com>
2022-03-30 17:15:44 +0300

    shir&rotem did: added the template of the chats screen

--- newicq/src/App.js
+++ newicq/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 import Login from './login/login';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+import Register from "./enroll/register";
+import MainScreenChats from './mainScreenChats/mainScreenChats';
 
 function App() {
   return (
     <div className="App">
-    <Login/>
+      <MainScreenChats/>
+      {/* 
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login/>}></Route>
+      <Route path='/enroll' element={<Register/>}></Route>
+    </Routes>
+    <div>Not register?
+      <Link to="/enroll" className="link-primary">Click here </Link>
+    
+      to register</div> 
+   
+    </BrowserRouter>
+   */}
     </div>
   );
 }
