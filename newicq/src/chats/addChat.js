b6fa585a929b0e38705e4bbea78168c30974ad3e
rotem <rotemg38@gmail.com>
2022-04-07 17:31:31 +0300

    1)handle all syntax warnings in /chats 2)finish integration -the support when click the chat show relevant messeges

--- newicq/src/chats/addChat.js
+++ newicq/src/chats/addChat.js
@@ -18,27 +18,27 @@ function AddChat(props) {
     };
     return (
         <div>
-            <ul class="list-group">
-                <li class="list-group-item">My User</li>
-                <button type="button" class="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Add chat</button>
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Add new contact</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <ul className="list-group">
+                <li className="list-group-item">My User</li>
+                <button type="button" className="btn btn-outline-dark" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Add chat</button>
+                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Add new contact</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
-                            <div class="modal-body">
+                            <div className="modal-body">
                                 <form>
-                                    <div class="mb-3">
-                                        <label for="recipient-name" class="col-form-label"></label>
-                                        <input type="text" class="form-control" placeholder="Contuct's identifier" id="contactname"></input>
+                                    <div className="mb-3">
+                                        <label htmlFor="recipient-name" className="col-form-label"></label>
+                                        <input type="text" className="form-control" placeholder="Contuct's identifier" id="contactname"></input>
                                     </div>
                                 </form>
                             </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button onClick={addConection} value="Update" type="button" class="btn btn-primary" data-bs-dismiss="modal">Add</button>
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button onClick={addConection} value="Update" type="button" className="btn btn-primary" data-bs-dismiss="modal">Add</button>
                             </div>
                         </div>
                     </div>
--- newicq/src/screenChat/screenChat.js
+++ newicq/src/screenChat/screenChat.js
@@ -5,50 +5,25 @@ import {getMsgsByChatId} from '../dbHandle/dbHardcoded';
 import { useState } from "react";
 
 
-function ScreenChat(chatInfo, {getCurrentChatId}) {
-    
-    var chatMessages= getMsgsByChatId(chatInfo.chatId);
-    var connectedUser = chatInfo.connectedUser;
-    /*const messages = [{text: "hello",date:"09:00", from:"user1", to:"user2"},
-    {text: "hello friend",date:"09:10", from:"user2", to:"user1"},
-    {text: "need to go",date:"09:15", from:"user1", to:"user2"},
-    {text: "kmd nkc dnskcmdlskcjnbdhscjnkdmscnjbdhscjnklmds cnjbdhscjnkmdcnbdhscjnldsncbdskjnc",date:"09:20", from:"user2", to:"user1"},
-    {text: "bye bye",date:"09:30", from:"user2", to:"user1"}]*/
-    
-    var messageList = chatMessages.map((msg, key)=>{
-        msg["connectedUser"] = connectedUser;
-        return <Message {...msg} key={key}/>
-    });
-
-    const [msgsComponents, setMsgsComponents] = useState(messageList);
-
-    const updateMessages = (msg)=>{
-        setMsgsComponents( msgs => [...msgs, <Message {...msg} key={msgs.length}/>]);
-    };
-    
+function ScreenChat(chatInfo) {
     return (
         <>
         {
             //add check if chatId = -1 need to show empty page
             chatInfo.chatId !== "-1" ?(
                 <>
-                <div className="row alert alert-dark">profile</div>
+                <div className="row alert alert-dark">profile {chatInfo.chatId}</div>
                 {/*<div className="position-absolute bottom-0 start-1 w-100">    position-relative */}
                
                 <div id="messagesList" className="row screenChats-content-msgs">
-                    {msgsComponents}
+                    {chatInfo.msgsComponents}
                 </div>
                
-                <SendMessage {...chatInfo} funcUpdate={updateMessages}/>
+                <SendMessage {...chatInfo} funcUpdate={chatInfo.updateMessages}/>
                 {/*</div>*/}
                 </>
             ):(<></>)}
-
-      
-       
         </>
-    
-    
     );
 }
 export default ScreenChat;
