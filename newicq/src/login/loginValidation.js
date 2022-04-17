c6996bd4f832ed0d9562f4883e113c24b85d5adc
rotem <rotemg38@gmail.com>
2022-04-18 02:16:21 +0300

    1)fixed issue in register- now all user enroll details are saved 2)and changed the user profile image to rounded img 3)and did small changes in design of signIn to match the signUp design

--- newicq/src/login/loginValidation.js
+++ newicq/src/login/loginValidation.js
@@ -1,27 +1,32 @@
  import { userIsExists, getUserPassword } from "../dbHandle/dbHardcoded"
  
  function correctPassword (user, password) {
-    if (getUserPassword(user) === password) {
+    if (userIsExists(user) && getUserPassword(user) === password) {
         return true
     }
     return false
  }
 
  export function checkValidation(key, value) {
+    var result = true;
     var user = document.getElementById("username")
      if (key === "userName") {
         if(value === "" || !userIsExists(value)){
             user.classList.remove("is-valid")
             user.classList.add("is-invalid")
             user.setCustomValidity('Wrong username')
-            return 
+            result = false
+            
         }
         else{
             user.classList.remove("is-invalid")
             user.classList.add("is-valid")
             user.setCustomValidity('')
-            return 
+
         }
+        //if username changed check the password also
+        key = "password"
+        value = document.getElementById("password").value
      }
      if (key === "password") {
         var pass = document.getElementById("password")
@@ -29,13 +34,13 @@
             pass.classList.remove("is-valid")
             pass.classList.add("is-invalid")
             pass.setCustomValidity('Wrong password')
-            return 
+            return false
         }
         else{
             pass.classList.remove("is-invalid")
             pass.classList.add("is-valid")
             pass.setCustomValidity('')
-            return 
+            return result
         }
      }
  }
